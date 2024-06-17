078a01bdfd0175467be00723f9a97e3779f7de34
Roilan Rodriguez Castillo <roilanrodriguez55@gmail.com>
2024-06-17 15:53:24 -0400

    Adding loader and fixing bug while fetch data from API

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,17 +1,22 @@
 import axios from "axios"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import "../styles/MediaCard-styles.css"
 import { AnimatePresence, motion } from "react-magic-motion"
+import Loader from 'react-loaders'
+import "../styles/Loader.scss"
 
 function MediaCard({ title, setHistorial, historial, setMovie, movie }) {
+  const [isLoading, setIsLoading] = useState(false)
+
   useEffect(() => {
     fetchMovieByName()
   }, [title])
 
   async function fetchMovieByName() {
+    setIsLoading(true)
     const APIKEY = "e03b7495"
     try {
-      const res = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=${APIKEY}`)
+      const res = await axios.get(`https://www.omdbapi.com/?t=${title}&apikey=${APIKEY}`)
       const { Title, Year, Director, Actors, imdbID, Poster, Language, Plot } =
         await res.data
 
@@ -33,6 +38,7 @@ function MediaCard({ title, setHistorial, historial, setMovie, movie }) {
     } catch (error) {
       console.log(error.message)
     }
+    setIsLoading(false)
   }
 
   function addHistorial(movie) {
@@ -41,24 +47,28 @@ function MediaCard({ title, setHistorial, historial, setMovie, movie }) {
 
   return (
     <AnimatePresence>
-      {movie.title && (
-        <motion.div
-          className="media-card"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <h2>Resultados</h2>
-          <div className="poster-container">
-            <img src={movie.poster} alt="" width={150} />
-          </div>
-          <p className="media-card__title">{movie.title}</p>
-          <p className="media-card__year">{movie.year} </p>
-          <p>Director: {movie.director} </p>
-          <p>Actors: {movie.actors}</p>
-          <p>Language: {movie.lang}</p>
-          <p>Description: {movie.description}</p>
-        </motion.div>
+      {isLoading ? (
+        <Loader type="ball-pulse" />
+      ) : (
+        movie.title && (
+          <motion.div
+            className="media-card"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <h2>Resultados</h2>
+            <div className="poster-container">
+              <img src={movie.poster} alt="" width={150} />
+            </div>
+            <p className="media-card__title">{movie.title}</p>
+            <p className="media-card__year">{movie.year} </p>
+            <p>Director: {movie.director} </p>
+            <p>Actors: {movie.actors}</p>
+            <p>Language: {movie.lang}</p>
+            <p>Description: {movie.description}</p>
+          </motion.div>
+        )
       )}
     </AnimatePresence>
   )
